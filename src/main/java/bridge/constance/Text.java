5674bf321aecfc633cb2c9e89880dece1080cac5
endlessmomo <soohyuk96@naver.com>
2022-11-18 12:14:20 +0900

    feat(Text) : 상수 문자열을 관리하는 Text 클래스 생성

---
+++ src/main/java/bridge/constance/Text.java
@@ -0,0 +1,10 @@
+package bridge.constance;
+
+public class Text {
+    public static final String START_GAME_MESSAGE= "다리 건너기 게임을 시작합니다.";
+    public static final String RETRY_OR_CLOSE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    public static final String CLEAR_OR_NOT = "게임 성공 여부 : ";
+    public static final String HOW_MANY_TIMES = "총 시도한 횟수 : ";
+    public static final String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
+    public static final String MOVE_TO_WHERE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+}
