4a986285c77616ee2bf7e34f81aae122fc9066e8
endlessmomo <soohyuk96@naver.com>
2022-11-18 13:47:21 +0900

    feat(InputView) : 클래스 내 메소드 작성 - 사용자로부터 다리의 길이를 입력 받음 - 사용자로부터 움직일 위치를 입력 받음 - 사용자로부터 재시작, 종료 여부를 입력 받음

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,12 @@
 package bridge.view;
 
+import bridge.constance.Text;
+import bridge.model.BridgeSize;
+import bridge.model.Command;
+import bridge.model.MoveControl;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +16,23 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(Text.INPUT_BRIDGE_LENGTH);
+        return new BridgeSize(readLine()).getBridgeSize();
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println(Text.MOVE_TO_WHERE);
+        return MoveControl.of(readLine());
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public boolean readGameCommand() {
+        System.out.println(Text.RETRY_OR_CLOSE);
+        return Command.isReplay(readLine());
     }
 }
