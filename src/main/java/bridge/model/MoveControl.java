ffffae22fb89b8efd6327e15ce413faf2df0198c
endlessmomo <soohyuk96@naver.com>
2022-11-18 13:07:27 +0900

    feat(MoveControl) : MoveControl Enum 클래스 생성

--- src/main/java/bridge/model/MoveControl.java
+++ src/main/java/bridge/model/MoveControl.java
@@ -2,11 +2,6 @@ package bridge.model;
 
 /* 움직임을 담당하는 기능 */
 
-import bridge.constance.CustomErrorMessage;
-
-import java.util.Arrays;
-import java.util.List;
-
 public enum MoveControl {
     MOVE_UP("U", 1),
     MOVE_DOWN("D", 0);
@@ -19,6 +14,35 @@ public enum MoveControl {
         this.inputMove = inputMove;
     }
 
+    public static String of(int fromNumberGenerator) {
+        if (fromNumberGenerator == MOVE_DOWN.inputMove) {
+            return MOVE_DOWN.inputMoveKey;
+        }
 
+        return MOVE_UP.inputMoveKey;
+    }
+
+    public static String of(String input) {
+        validate(input);
+        return input;
+    }
+
+    private static void validate(String input) {
+        checkUpperCase(input);
+        checkCorrectInput(input);
+    }
+
+    private static void checkUpperCase(String input) {
+        if (!input.equals(input.toUpperCase())) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkCorrectInput(String input) {
+        if(!(input.equals(MOVE_UP.inputMoveKey)
+                || input.equals(MOVE_DOWN.inputMoveKey))){
+            throw new IllegalArgumentException();
+        }
+    }
 }
 
